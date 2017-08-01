38e90d5c8ac18cb7264469479e530c972b137f5b
annesteenbeek <annesteenbeek@gmail.com>
2017-08-01 22:06:13 +0000

    source dev env

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ set -e # exit if returns nonzero
 
 # setup ROS env
 source "${ROS_INSTALL_DIR}/setup.bash"
+source "${CATKIN_DIR}/dev/setup.bash"
 
 # setup ROS networking
 export ROS_HOSTNAME="localhost"
